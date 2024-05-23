d72f6581a8d34935afe7ed94d687e3be1e8fb730
Trupesh Patel <pateltrupesh27@gmail.com>
2024-05-23 16:08:15 -0500

    Fixed ulimit and added missing *.TBL fils

--- singularity/templates/guide/HelloNGEN.sh
+++ singularity/templates/guide/HelloNGEN.sh
@@ -8,6 +8,15 @@ MAGENTA='\e[35m'
 CYAN='\e[36m'
 RESET='\e[0m'
 
+# Increasing `ulimit` to Open files
+ulimit -n 500000
+
+# Loading Lmod
+source /etc/profile.d/modules.sh
+
+# Loading OpenMPI module for Parallel Run
+module load mpi
+
 workdir="${1:-/ngen}"
 cd "${workdir}" || { echo -e "${RED}Failed to change directory to ${workdir}${RESET}"; exit 1; }
 set -e
