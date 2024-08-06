6eb2f7d03d0bb386735ced405865c116191909f3
Benjamin Lee <130000039+benlee0423@users.noreply.github.com>
2024-08-06 15:36:55 -0700

    Update install_t_route.sh

--- singularity/templates/t-route/install_t_route.sh
+++ singularity/templates/t-route/install_t_route.sh
@@ -25,19 +25,23 @@ git submodule update --init
 
 cd /t-route/src/troute-network
 python setup.py --use-cython bdist_wheel
+cp /t-route/src/troute-network/dist/*.whl /t-route/wheels
 
 cd /t-route/src/troute-routing
 python setup.py --use-cython bdist_wheel
+cp /t-route/src/troute-routing/dist/*.whl /t-route/wheels
 
 # troute-config doesn't use setup.py, use build to make the wheel
 cd /t-route/src/troute-config
 python -m build . \
+cp /t-route/src/troute-routing/dist/*.whl /t-route/wheels
 
 cd /t-route/src/troute-nwm
 python -m build . \
-
-cd /t-route
-cp /t-route/src/troute-network/dist/*.whl /t-route/wheels
-cp /t-route/src/troute-routing/dist/*.whl /t-route/wheels
-cp /t-route/src/troute-config/dist/*.whl /t-route/wheels
 cp /t-route/src/troute-nwm/dist/*.whl /t-route/wheels
+
+#cd /t-route
+#cp /t-route/src/troute-network/dist/*.whl /t-route/wheels
+#cp /t-route/src/troute-routing/dist/*.whl /t-route/wheels
+#cp /t-route/src/troute-config/dist/*.whl /t-route/wheels
+#cp /t-route/src/troute-nwm/dist/*.whl /t-route/wheels
