0e2d075a8e4572e45e82d5081df6c5bd9c235456
romer8 <gio.rombus@gmail.com>
2025-04-04 12:42:55 -0600

    cahnges

--- viewOnTethys.sh
+++ viewOnTethys.sh
@@ -323,23 +323,7 @@ _add_model_run() {
 
   # Always use /var/lib/tethys_persist/ngiab_visualizer as the base directory
   local final_path="/var/lib/tethys_persist/ngiab_visualizer/$base_name"
-
-  jq --arg label "$base_name" \
-     --arg path  "$final_path" \
-     --arg date  "$current_time" \
-     --arg id    "$new_uuid" \
-     '.model_runs += [ 
-       { 
-         "label": $label, 
-         "path": $path, 
-         "date": $date, 
-         "id": $id, 
-         "subset": "", 
-         "tags": [] 
-       }
-     ]' \
-     "$json_file" > "${json_file}.tmp" && mv -f "${json_file}.tmp" "$json_file"
-}
+  jq --arg label "$base_name" --arg path "$final_path" --arg date "$current_time" --arg id "$new_uuid" '.model_runs += [{ "label": $label, "path": $path, "date": $date, "id": $id, "subset": "", "tags": [] }]' "$json_file" > "${json_file}.tmp" && mv -f "${json_file}.tmp" "$json_file"
 
 
 create_tethys_portal(){
